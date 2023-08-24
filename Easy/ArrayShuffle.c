8f6aaafd916a07e0e0ec7385c1eb95a37d7405f9
snowwhitesheep <snw.wht.lk@gmail.com>
2023-08-24 17:07:37 +0200

    1470 first try

--- Easy/ArrayShuffle.c
+++ Easy/ArrayShuffle.c
@@ -2,15 +2,33 @@
 //Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 //Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
-int*shuffle(int*,int,int,int*);
+int*shuffle(int*,int,int);
 
 int main(){
+    int initial[] = {2,5,1,3,4,7};
+    int n = 3;
+
+    int shuffled[] = suffle(initial, 6, n);
+
+    for(int i = 0; i<6; i++){
+        printf("%d ", shuffled[i]);
+    }
 
     return 0;
 }
-int* shuffle(int*nums, int numsSize, int n, int*returnSize){
+
+
+int* shuffle(int*nums, int numsSize, int n){
     int*returnNums;
-    returnNums = (int*)malloc(*returnSize * sizeof(int));
+
+    returnNums = (int*)malloc(numsSize * sizeof(int));
+    for(int i,j = 0; i < n; i++, j+=2){
+        returnNums[j] = nums[i];
+    }
+    int j = 1;
+    for(int i = n; i < numsSize; i++, j+=2){
+        returnNums[j] = nums[i];
+    }
     
 
     return returnNums;
