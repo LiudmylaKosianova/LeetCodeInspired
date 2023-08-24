38e4574d85d82f4ca6d49726f582112dcf824006
snowwhitesheep <snw.wht.lk@gmail.com>
2023-08-24 16:50:47 +0200

    1470 start

---
+++ Easy/ArrayShuffle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+//Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+int*shuffle(int*,int,int,int*);
+
+int main(){
+
+    return 0;
+}
+int* shuffle(int*nums, int numsSize, int n, int*returnSize){
+    int*returnNums;
+    returnNums = (int*)malloc(*returnSize * sizeof(int));
+    
+
+    return returnNums;
+
+}
